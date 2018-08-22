959f429197df6c6f07e9d7e0f0e3f59ae30011fa
José Alexandre D'Abruzzo Pereira <josep@dei.uc.pt>
2018-08-22 16:52:52 +0100

    Changing the primary key to auto_increment

--- database/TMA-K_create_database.sql
+++ database/TMA-K_create_database.sql
@@ -1,80 +1,67 @@
 CREATE TABLE Actuator (
- actuatorId INT NOT NULL,
+ actuatorId MEDIUMINT NOT NULL AUTO_INCREMENT,
  address VARCHAR(1024),
- pubKey VARCHAR(1024)
+ pubKey VARCHAR(1024),
+ PRIMARY KEY (actuatorId)
 );
-
-ALTER TABLE Actuator ADD CONSTRAINT PK_Actuator PRIMARY KEY (actuatorId);
-
 
 CREATE TABLE Description (
- descriptionId INT NOT NULL,
+ descriptionId MEDIUMINT NOT NULL AUTO_INCREMENT,
  dataType CHAR(16),
  descriptionName CHAR(128),
- unit CHAR(16)
+ unit CHAR(16),
+ PRIMARY KEY (descriptionId)
 );
 
-ALTER TABLE Description ADD CONSTRAINT PK_Description PRIMARY KEY (descriptionId);
-
-
 CREATE TABLE Probe (
- probeId INT NOT NULL,
+ probeId MEDIUMINT NOT NULL AUTO_INCREMENT,
  probeName VARCHAR(128),
  password VARCHAR(128),
  salt VARCHAR(128) NOT NULL,
  token VARCHAR(256),
- tokenExpiration TIMESTAMP(10)
+ tokenExpiration TIMESTAMP(6),
+ PRIMARY KEY (probeId)
 );
-
-ALTER TABLE Probe ADD CONSTRAINT PK_Probe PRIMARY KEY (probeId);
 
 
 CREATE TABLE Resource (
- resourceId INT NOT NULL,
+ resourceId MEDIUMINT NOT NULL AUTO_INCREMENT,
  resourceName VARCHAR(128),
  resourceType VARCHAR(16),
- resourceAddress VARCHAR(1024)
+ resourceAddress VARCHAR(1024),
+ PRIMARY KEY (resourceId)
 );
-
-ALTER TABLE Resource ADD CONSTRAINT PK_Resource PRIMARY KEY (resourceId);
 
 
 CREATE TABLE Time (
- valueTime TIMESTAMP(10) NOT NULL
+ valueTime TIMESTAMP(6) NOT NULL,
+ PRIMARY KEY (valueTime)
 );
-
-ALTER TABLE Time ADD CONSTRAINT PK_Time PRIMARY KEY (valueTime);
 
 
 CREATE TABLE Action (
- actionId INT NOT NULL,
+ actionId MEDIUMINT NOT NULL AUTO_INCREMENT,
  actuatorId INT NOT NULL,
  resourceId INT NOT NULL,
- actionName VARCHAR(128)
+ actionName VARCHAR(128),
+ PRIMARY KEY (actionId)
 );
-
-ALTER TABLE Action ADD CONSTRAINT PK_Action PRIMARY KEY (actionId);
-
 
 CREATE TABLE Configuration (
  actionId INT NOT NULL,
- key VARCHAR(128),
- domain VARCHAR(1024)
+ keyName VARCHAR(128),
+ domain VARCHAR(1024),
+ PRIMARY KEY (actionId)
 );
-
-ALTER TABLE Configuration ADD CONSTRAINT PK_Configuration PRIMARY KEY (actionId);
-
 
 CREATE TABLE Data (
  probeId INT NOT NULL,
  descriptionId INT NOT NULL,
  resourceId INT NOT NULL,
- valueTime TIMESTAMP(10) NOT NULL,
- value DOUBLE PRECISION
+ valueTime TIMESTAMP(6) NOT NULL,
+ value DOUBLE PRECISION,
+ PRIMARY KEY (probeId,descriptionId,resourceId,valueTime)
 );
-
-ALTER TABLE Data ADD CONSTRAINT PK_Data PRIMARY KEY (probeId,descriptionId,resourceId,valueTime);
-
 
 ALTER TABLE Action ADD CONSTRAINT FK_Action_0 FOREIGN KEY (actuatorId) REFERENCES Actuator (actuatorId);
 ALTER TABLE Action ADD CONSTRAINT FK_Action_1 FOREIGN KEY (resourceId) REFERENCES Resource (resourceId);
@@ -87,5 +74,3 @@ ALTER TABLE Data ADD CONSTRAINT FK_Data_0 FOREIGN KEY (probeId) REFERENCES Probe
 ALTER TABLE Data ADD CONSTRAINT FK_Data_1 FOREIGN KEY (descriptionId) REFERENCES Description (descriptionId);
 ALTER TABLE Data ADD CONSTRAINT FK_Data_2 FOREIGN KEY (resourceId) REFERENCES Resource (resourceId);
 ALTER TABLE Data ADD CONSTRAINT FK_Data_3 FOREIGN KEY (valueTime) REFERENCES Time (valueTime);
-
-
