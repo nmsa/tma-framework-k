969006a7f0bd10426159633ec5d0f3f3ca99471c
Rui Silva <rslv560@gmail.com>
2018-09-07 10:00:05 +0100

    Add Script to create Example Data

---
+++ development/mysql/example_data_mysql.sh
@@ -0,0 +1,18 @@
+timestamp() {
+	date +'%F %T'
+}
+
+
+i="0"
+
+while [ $i -lt 141 ]
+do
+time=\'$(timestamp)\'
+kubectl exec -ti mysql-0 -- mysql -u root -ppassword <<QUERY_INPUT
+use teste;
+INSERT INTO Probe(probeId,probeName,password,salt,token,tokenExpiration) VALUES ($i,"probe wildfly WSVD", "a","a","a",${time});
+INSERT INTO Resource(resourceId,resourceName,resourceType,resourceAddress) VALUES ($i,"VM_VIRT_NODE", "a","a");
+INSERT INTO Description(descriptionId,dataType,descriptionName,unit) VALUES ($i,"CPU_Usage", "measurement","Mi");
+QUERY_INPUT
+i=$((i+1))
+done
