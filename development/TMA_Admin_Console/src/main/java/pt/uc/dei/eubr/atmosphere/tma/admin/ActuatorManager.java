c8b06872cd3ee91aa8585658ad16b72b53af76a4
Jose Pereira <josep@dei.uc.pt>
2018-09-07 16:43:56 +0100

    Initial commit of the project in a new location

---
+++ development/TMA_Admin_Console/src/main/java/pt/uc/dei/eubr/atmosphere/tma/admin/ActuatorManager.java
@@ -0,0 +1,93 @@
+package pt.uc.dei.eubr.atmosphere.tma.admin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedTreeMap;
+
+public class ActuatorManager {
+
+	public Long saveNewActuator(String address, String pubKey) {
+		String sql = "INSERT INTO Actuator(address, pubKey) VALUES (?, ?)";
+		PreparedStatement ps = null;
+		
+		try {
+			ps = DatabaseManager.getConnectionInstance().prepareStatement(sql);
+			ps.setString(1, address);
+			ps.setString(2, pubKey);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		DatabaseManager databaseManager = new DatabaseManager();
+		return databaseManager.executeQuery(ps);
+	}
+	
+	public void saveActions() {
+	    
+		//String jsonString = readJson();
+		readJson2();
+		/*JsonObject obj = new JsonObject(jsonString);
+		JsonArray arr = obj.getAsJsonArray()("number");
+		for (int i = 0; i < arr.length(); i++)
+		    System.out.println(arr.getInt(i));*/
+	}
+	
+    private void readJson2() {
+    	Gson gson = new GsonBuilder().create();
+    	
+    	String filename = "/Users/josealexandredabruzzopereira/Projects/"
+    	+ "TMA_Admin_Console/src/main/resources/actions.json";
+    	File file = new File(filename);
+		InputStream input;
+		try {
+			input = new FileInputStream(filename);
+			InputStreamReader isr = new InputStreamReader(input);
+	
+			Object rawJson = gson.fromJson(isr, Object.class);	  
+			LinkedTreeMap<String, Object> c = (LinkedTreeMap<String, Object>) rawJson;
+			List<Object> actions = (List<Object>) c.get("actions");
+			for (Object object : actions) {
+				LinkedTreeMap<String, Object> actionData = (LinkedTreeMap<String, Object>) object;
+				System.out.println(actionData);	
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+	
+	private String readJson() {
+		StringBuilder sb = new StringBuilder();
+		try {
+			// pass the path to the file as a parameter 
+		    FileReader fr = 
+		      new FileReader("C:\\Users\\pankaj\\Desktop\\test.txt"); 
+
+            int i;
+		    while ((i=fr.read()) != -1) {
+			  System.out.print((char) i);
+			  sb.append((char) i);
+		    }
+		    
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+	
+	
+}
---
+++ development/TMA_Admin_Console/src/main/java/pt/uc/dei/eubr/atmosphere/tma/admin/DatabaseManager.java
@@ -0,0 +1,105 @@
+package pt.uc.dei.eubr.atmosphere.tma.admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+    private static Connection connection = null;
+    private Statement statement = null;
+    private ResultSet resultSet = null;
+
+    private static Connection getConnectionInstanceMySQL() {
+        // This will load the MySQL driver, each DB has its own driver
+        try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        // Setup the connection with the DB
+        try {
+			connection = DriverManager
+			        .getConnection("jdbc:mysql://localhost/feedback?"
+			                + "user=sqluser&password=sqluserpw");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return connection;
+    }
+    
+    public static Connection getConnectionInstance() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:/Users/josealexandredabruzzopereira/"
+        		+ "Projects/tma-framework-k/development/TMA-Admin/sqlite/db/"
+        		+ "adminDatabase";
+
+        try {
+            if ((connection == null) || connection.isClosed()) {
+            	connection = DriverManager.getConnection(url);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+    }
+
+    static void executeStatement(String sql) {
+        Connection conn = getConnectionInstance();
+        try {
+            Statement statement = conn.createStatement();
+            statement.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static ResultSet executeQuery(String sql) {
+        Connection conn = getConnectionInstance();
+        Statement stmt;
+        ResultSet rs = null;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+	public Long executeQuery(PreparedStatement ps) {
+		Long key = (long) -1;
+		try {
+			ps.executeQuery();
+			ResultSet generatedKeys = ps.getGeneratedKeys();
+			if (generatedKeys.next()) {
+				key = generatedKeys.getLong(1);
+				System.out.println(key);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+	    }
+		return key;
+	}
+}
