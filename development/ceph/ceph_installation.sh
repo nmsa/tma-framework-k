73356a53159e4392bfc18c03d8d5ea29ec2d8891
Gyelo <rslv560@gmail.com>
2018-09-11 17:20:27 +0100

    Fix and Comment Ceph Scripts

--- development/ceph/ceph_installation.sh
+++ development/ceph/ceph_installation.sh
@@ -1,17 +1,20 @@
+#Install ceph
 apt-get -y install ceph
+
+#Install docker
 apt-get -y install docker.io
+
+#Pull ceph Docker image from Docker Hub
 docker pull docker.avvo.com/ceph-demo:luminous
+
+#Remove some conflicting files 
 rm -rf /etc/ceph/*
 rm -rf /var/lib/ceph/*
+
+# Give permissions to container can write in necessary directories
 chmod 777 /etc/ceph
 chmod 777 /var/lib/ceph
+
+# Deploy Ceph container
 docker run --net=host -v /etc/ceph:/etc/ceph -v /var/lib/ceph:/var/lib/ceph \
   -e MON_IP=192.168.1.1 -e CEPH_PUBLIC_NETWORK=192.168.1.0/24 docker.avvo.com/ceph-demo:luminous
-rbd create tma_k_db_mysql -s 1024
-rbd feature disable tma_k_db_mysql fast-diff
-rbd feature disable tma_k_db_mysql object-map
-rbd feature disable tma_k_db_mysql deep-flatten
-mkfs.ext4 /dev/rbd0
-ceph auth get-key client.admin > temp.txt
-key="$(sed -n 1p temp.txt)"
-echo "${key}"| base64
