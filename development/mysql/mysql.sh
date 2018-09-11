5c0d33250f8868efee1fcf6955d1b1abc10387d5
Gyelo <rslv560@gmail.com>
2018-09-11 17:07:46 +0100

    Remove MySQL script

--- development/mysql/mysql.sh
+++
@@ -1,5 +0,0 @@
-kubectl exec -ti mysql-0 -- mysql -u root -ppassword -e "CREATE DATABASE knowledge /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-kubectl exec -ti mysql-0 -- mysql -u root -ppassword <<QUERY_INPUT
-use knowledge;
-CREATE TABLE measurements (probeId INT(20), resourceId INT(20), type VARCHAR(20), descriptionId INT(20), time INT(20), value FLOAT(20));
-QUERY_INPUT
