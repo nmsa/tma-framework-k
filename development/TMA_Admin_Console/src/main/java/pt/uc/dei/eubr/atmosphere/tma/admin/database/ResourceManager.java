8c380c6fc07d17746a56b162d4a140ec8c17ebc6
José Alexandre D'Abruzzo Pereira <joseadp@gmail.com>
2018-09-13 11:46:25 +0100

    Admin Actuator (#7)
    * Initial Admin Console structure
    * Feature to encrypt/decrypt message
    * Initial structure to add new actuators and actions

---
+++ development/TMA_Admin_Console/src/main/java/pt/uc/dei/eubr/atmosphere/tma/admin/database/ResourceManager.java
@@ -0,0 +1,30 @@
+package pt.uc.dei.eubr.atmosphere.tma.admin.database;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+
+public class ResourceManager {
+	
+	public int saveNewResource(String name, String type, String address) {
+		String sql = "INSERT INTO "
+				+ "Resource(resourceName, resourceType, resourceAddress) "
+				+ "VALUES "
+				+ "(?, ?, ?)";
+		
+		PreparedStatement ps = null;
+		
+		try {
+			ps = DatabaseManager.getConnectionInstance().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, name);
+			ps.setString(2, type);
+			ps.setString(3, address);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		DatabaseManager databaseManager = new DatabaseManager();
+		return databaseManager.execute(ps);
+	}
+}
