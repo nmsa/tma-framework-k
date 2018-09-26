254a2b69055c30b63c898db72cd8e319c47c50a4
Rui Silva <rslv560@gmail.com>
2018-09-26 12:54:35 +0100

    Script timeout improvement

--- development/mysql/setup_database.sh
+++ development/mysql/setup_database.sh
@@ -2,8 +2,15 @@
 kubectl create -f mysql-deployment.yaml
 
 # Timeout until MySQL pod is ready
-
-sleep 25
+getmyqlstate () {
+	mysqlstate=$(kubectl get pods -n default mysql-0 -o jsonpath="{.status.phase}")
+}
+getmyqlstate
+while [ $mysqlstate != "Running" ]
+do
+getmyqlstate
+sleep 1
+done
 
 # Creation of knowledge in MySQL pod
 kubectl exec -ti mysql-0 -- mysql -u root -ppassword -e "CREATE DATABASE knowledge /*\!40100 DEFAULT CHARACTER SET utf8 */;"
