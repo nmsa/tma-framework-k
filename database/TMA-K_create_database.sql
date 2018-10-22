4c07c966ea4cb38a36b34cfe82edc80c3e69c6a5
Jose Pereira <josep@dei.uc.pt>
2018-10-22 15:41:55 +0100

    Updated normalized version of database creation (Metric tables)

--- database/TMA-K_create_database.sql
+++ database/TMA-K_create_database.sql
@@ -20,6 +20,12 @@
 -- ------------------------------------------------------------------------------
 -- ------------------------------------------------------------------------------
 
+DROP TABLE IF EXISTS MetricData;
+DROP TABLE IF EXISTS MetricComponent;
+
+DROP TABLE IF EXISTS Metric;
+DROP TABLE IF EXISTS QualityModel;
+
 DROP TABLE IF EXISTS Data;
 DROP TABLE IF EXISTS Configuration;
 
@@ -57,6 +63,11 @@ CREATE TABLE Probe (
     PRIMARY KEY (probeId)
 );
 
+CREATE TABLE QualityModel (
+    qualityModelId VARCHAR(10) NOT NULL AUTO_INCREMENT,
+    modelName VARCHAR(10),
+    PRIMARY KEY (qualityModelId)
+);
 
 CREATE TABLE Resource (
     resourceId INT NOT NULL AUTO_INCREMENT,
@@ -89,6 +100,38 @@ CREATE TABLE Configuration (
     PRIMARY KEY (configurationId)
 );
 
+CREATE TABLE Metric (
+    metricId INT NOT NULL AUTO_INCREMENT,
+    qualityModelId VARCHAR(10) NOT NULL,
+    normalizationKind VARCHAR(10),
+    metricName VARCHAR(10),
+    metricAggregationOperator INT,
+    threshold DOUBLE PRECISION,
+    blockLevel INT,
+    PRIMARY KEY (metricId)
+);
+
+CREATE TABLE MetricData (
+    metricId INT NOT NULL,
+    valueTime TIMESTAMP(10) NOT NULL,
+    qualityModelId VARCHAR(10) NOT NULL,
+    value DOUBLE PRECISION,
+    resourceId INT NOT NULL,
+    PRIMARY KEY (metricId,valueTime,qualityModelId)
+)
+
+CREATE TABLE MetricComponent (
+    descriptionId INT NOT NULL,
+    metricId INT NOT NULL,
+    qualityModelId VARCHAR(10) NOT NULL,
+    attributeAggregationOperator INT,
+    numSamples INT,
+    weight DOUBLE PRECISION,
+
+    PRIMARY KEY (descriptionId,metricId,qualityModelId)
+);
+
+
 CREATE TABLE Data (
     probeId INT NOT NULL,
     descriptionId INT NOT NULL,
@@ -103,13 +146,18 @@ ALTER TABLE Action ADD CONSTRAINT FK_Action_1 FOREIGN KEY (resourceId) REFERENCE
 
 ALTER TABLE Configuration ADD CONSTRAINT FK_Configuration_0 FOREIGN KEY (actionId) REFERENCES Action (actionId);
 
-ALTER TABLE Data ADD CONSTRAINT FK_Data_0 FOREIGN KEY (probeId) REFERENCES Probe (probeId);
-ALTER TABLE Data ADD CONSTRAINT FK_Data_1 FOREIGN KEY (descriptionId) REFERENCES Description (descriptionId);
-ALTER TABLE Data ADD CONSTRAINT FK_Data_2 FOREIGN KEY (resourceId) REFERENCES Resource (resourceId);
-
 -- -- Table time was removed for normalization.
 -- ALTER TABLE Data ADD CONSTRAINT FK_Data_3 FOREIGN KEY (valueTime) REFERENCES Time (valueTime);
 
+ALTER TABLE Metric ADD CONSTRAINT FK_Metric_0 FOREIGN KEY (qualityModelId) REFERENCES QualityModel (qualityModelId);
 
+ALTER TABLE MetricData ADD CONSTRAINT FK_MetricData_0 FOREIGN KEY (metricId,qualityModelId) REFERENCES Metric (metricId,qualityModelId);
+ALTER TABLE MetricData ADD CONSTRAINT FK_MetricData_1 FOREIGN KEY (resourceId) REFERENCES Resource (resourceId);
 
+ALTER TABLE MetricComponent ADD CONSTRAINT FK_MetricComponent_0 FOREIGN KEY (descriptionId) REFERENCES Description (descriptionId);
+ALTER TABLE MetricComponent ADD CONSTRAINT FK_MetricComponent_1 FOREIGN KEY (metricId,qualityModelId) REFERENCES Metric (metricId,qualityModelId);
+
+ALTER TABLE Data ADD CONSTRAINT FK_Data_0 FOREIGN KEY (probeId) REFERENCES Probe (probeId);
+ALTER TABLE Data ADD CONSTRAINT FK_Data_1 FOREIGN KEY (descriptionId) REFERENCES Description (descriptionId);
+ALTER TABLE Data ADD CONSTRAINT FK_Data_2 FOREIGN KEY (resourceId) REFERENCES Resource (resourceId);
 
