8ca78b42bd5eac5e427d0bbf3e38bd2187e78036
Jodao <49023295+Jodao@users.noreply.github.com>
2022-02-25 15:39:38 +0000

    Debugging purposes

--- development/tma-admin-api/src/main/java/eu/atmosphere/tma/admin/controller/MetricController.java
+++ development/tma-admin-api/src/main/java/eu/atmosphere/tma/admin/controller/MetricController.java
@@ -5,26 +5,12 @@
  */
 package eu.atmosphere.tma.admin.controller;
 
-import eu.atmosphere.tma.admin.dto.MetricDashboard;
-import eu.atmosphere.tma.admin.util.Constants;
-import eu.atmosphere.tma.admin.util.DatabaseManager;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -37,47 +23,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class MetricController {
     
     private static final Logger LOGGER = LoggerFactory.getLogger(MetricController.class);
-
-    @GetMapping("/getMetrics")
-    public ResponseEntity<Map> getMetrics(@RequestParam(name = "filter") String filter, HttpServletResponse response) {
-        DatabaseManager database = new DatabaseManager();
-        ArrayList<MetricDashboard> metrics;
-        try {
-            metrics = database.getMetrics(filter);
-        } catch (SQLException ex) {
-            LOGGER.error("[ATMOSPHERE] Unable to connect to the database", ex);
-            return AdminController.genericResponseEntity(Constants.HTTPSERVERERROR,
-                    Constants.ERROR, "There was a problem with the connection to the database");
-        }
-
-        HashMap<String, ArrayList<MetricDashboard>> probesJson = new HashMap<>();
-        probesJson.put("metrics", metrics);
-
-        return new ResponseEntity<>(
-                probesJson,
-                HttpStatus.valueOf(response.getStatus())
-        );
-    }
-    
-    @GetMapping("/getMetrics/{id}")
-    public ResponseEntity<Map> getMetricById(@PathVariable(name= "id") int id, HttpServletResponse response) {
-        DatabaseManager database = new DatabaseManager();
-        MetricDashboard metric;
-        try {
-            metric = database.getMetricById(id);
-        } catch (SQLException ex) {
-            LOGGER.error("[ATMOSPHERE] Unable to connect to the database", ex);
-            return AdminController.genericResponseEntity(Constants.HTTPSERVERERROR,
-                    Constants.ERROR, "There was a problem with the connection to the database");
-        }
-
-        HashMap<String, MetricDashboard> metricJson = new HashMap<>();
-        metricJson.put("metric", metric);
-
-        return new ResponseEntity<>(
-                metricJson,
-                HttpStatus.valueOf(response.getStatus())
-        );
-    }
-
 }
